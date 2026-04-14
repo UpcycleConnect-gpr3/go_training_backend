8bf991f06d9cb4c7b549c9652d85f387b5f5643a
FortyTwo-Dev <91018423+FortyTwo-Dev@users.noreply.github.com>
2026-04-14 22:19:56 +0200

    add

--- app/handlers/metric_handlers/health.go
+++ app/handlers/metric_handlers/health.go
@@ -1,7 +1,10 @@
 package metric_handlers
 
-import "net/http"
+import (
+	"go-upcycle_connect-backend/utils/log"
+	"net/http"
+)
 
 func Health(w http.ResponseWriter, r *http.Request) {
-
+	log.Api(r)
 }
